90cb6fb121aad210f984fc08cdbee98591b79f9e
sinisa <miskovic.sinisa44@gmail.com>
2020-11-05 12:35:29 +0100

    CRUD over

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ if( process.env.NODE_ENV === 'development' ) {
     app.use(morgan('dev'));
 }
 
+app.use(express.json());
 
 app.use('/api/users', userRoutes );
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,41 +1,90 @@
 const User = require( './../models/User' );
 
-exports.index = (req,res) => {
-    res.status(200).json(
-        {
-            message:'index'
-        }
-    )
+exports.index = async(req,res) => {
+    try {
+        const users =await User.find();
+
+        res.status(200).json({
+            count: users.length,
+            data:{
+                users
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            message:err
+        })
+    }
 }
 
-exports.show = (req,res) => {
-    res.status(200).json(
-        {
-            message: 'show'
-        }
-    )
+exports.show = async (req,res) => {
+    try {
+        const user = await User.findById( req.params.id );
+
+        res.status(200).json({
+            data:{ user }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'error',
+            message: error
+        })
+    }
 }
 
-exports.create = (req, res) => {
-    res.status(200).json(
-        {
-            message:'create'
-        }
-    )
+exports.create = async (req, res) => {
+    console.log(req.body);
+   try {
+       const newUser = await User.create( req.body );
+
+       res.status(201).json({
+           status:'success',
+           data:{
+               user:newUser
+           }
+       })
+   } catch (error) {
+       res.status(400).json({
+           status:'fail',
+           message:error
+       })
+   }
 }
 
-exports.update = ( req, res) => {
-    res.status(200).json(
-        {
-            message:'update'
-        }
-    )
+exports.update = async( req, res) => {
+    try {
+        const user = await User.findOneAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators:true
+        });
+
+        res.status(200).json({
+            status:'success',
+            data:{
+                user
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            message:error
+        })        
+    }
 }
 
-exports.delete = ( req, res) => {
-    res.status( 200 ).json(
-        {
-            message: 'delete'
-        }
-    )
+exports.delete = async (req, res) => {
+   try{
+       await User.findOneAndDelete( req.params.id );
+
+       res.status(204).json({
+           status:'success',
+           data:null
+       })
+   } catch( error ) {
+       res.status(404).json({
+           status:'fail',
+           message:error
+       })
+   }
 }
